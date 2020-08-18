2625c456008d4845b6872e3d32434700a15f25a9
syaedisyafrilnizam <64898726+syaedisyafrilnizam@users.noreply.github.com>
2020-08-18 15:18:16 +0900

    Minor column property change & product dummy data

--- dbsetup.js
+++ dbsetup.js
@@ -72,7 +72,7 @@ app.get("/cr_tb_product", function(req, res) {
   	product_id int unsigned not null auto_increment,
   	product varchar(100) not null,
     type_avail varchar(100),
-  	info varchar(300),
+  	info text,
   	price int unsigned not null,
   	user_id varchar(30) not null,
   	rating double unsigned,
@@ -92,6 +92,15 @@ app.get("/cr_tb_product", function(req, res) {
     res.send("Table created..");
   })
 });
+
+/* product 테이블의 더미 데이터 */
+// INSERT INTO `product`(`product_id`, `product`, `type_avail`, `info`, `price`, `user_id`, `rating`, `quantity`, `category`) VALUES (1, "Porstina - Embroidered Zip Jacket", "Black,White", "Oversized jacket accented with durable metallic zipper at the center front and minimal embroidery ooze streetwear vibes. Its drop-shoulder cut is accentuated with cinched elastic cuffs and roomy welt pockets. Available in a range of sizes.", 23356, "Porstina", "4.6", 57, "Clothing");
+//
+// INSERT INTO `product`(`product_id`, `product`, `type_avail`, `info`, `price`, `user_id`, `rating`, `quantity`, `category`) VALUES (2, "HANO - Lace-Up Short Boots", "Black,Dark Blue", "A boot, plural boots, is a type of specific footwear. Most boots mainly cover the foot and the ankle, while some also cover some part of the lower calf. Some boots extend up the leg, sometimes as far as the knee or even the hip. Most boots have a heel that is clearly distinguishable from the rest of the sole, even if the two are made of one piece. Traditionally made of leather or rubber, modern boots are made from a variety of materials. Boots are worn both for their functionality – protecting the foot and leg from water, extreme cold, mud or hazards (e.g., work boots may protect wearers from chemicals or use a steel toe) or providing additional ankle support for strenuous activities with added traction requirements (e.g., hiking), or may have hobnails on their undersides to protect against wear and to get better grip; and for reasons of style and fashion.", 109153, "Hano", "3.4", 24, "Shoes");
+//
+// INSERT INTO `product`(`product_id`, `product`, `type_avail`, `info`, `price`, `user_id`, `rating`, `quantity`, `category`) VALUES (3, "Carryme - Set: Lightweight Backpack + Pouch", "Green,Khaki", "This pretty drawstring bag has a perfect size for transporting man's or woman's swim gear or PE kit or other essentials for the gym, school and other short trips. Put it on your back and you won't even notice it's there when out on short excursions or bike trips..", 54208, "Trencur", "5.0", 20, "Bags");
+//
+// INSERT INTO `product`(`product_id`, `product`, `type_avail`, `info`, `price`, `user_id`, `rating`, `quantity`, `category`) VALUES (4, "Aisyi - Blue Light Blocking Glasses", "Red,Gold,Brown", "As practical as they’re stylish, these black-rimmed glasses with thin but study frames are fitted with blue light blocking technology to protect the eyes. Perfect for long days in front of the computer as well as for quick pre-bedtime peeks at your phone!", 14372, "Trencur", "2.6", 192, "Accessories");
 
 // seller 테이블 생성
 app.get("/cr_tb_seller", function(req, res) {
